9c42a4857bd9a45cf7a5f4eff4efa6f9298c4c11
Joana Sousa <49571850+jbtsousa@users.noreply.github.com>
2019-12-08 19:17:34 +0000

    Add files via upload

--- ucbusinesssite/static/ucbusinesssite/javascript/index.js
+++ ucbusinesssite/static/ucbusinesssite/javascript/index.js
@@ -1,14 +1,44 @@
 window.onload = function () {
+        var data = {
+          "6": "Inauguração Vice-Reitoria Pólo II e Lançamento do UC Business e Lançamento ASUC Ensino Secundário",
+          "11": "Apresentação do projeto Fit4work"
+        }
+
         var node = document.getElementById("rectangle")
         var months= ["jan","fev","mar","apr","may","jun","jul","aug","set","oct","nov", "dec"]
         var days=["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31"]
+        var numdays=31;
+        var rectangle_message = document.querySelector("#rectangle_message")
 
-        for (var i = 0; i < 12; ++i) {
+        for (var i = 0; i < numdays; ++i) {
                 var circle = document.createElement("div")
                 var text = document.createElement("span")
-                text.innerHTML = months[i]
-                circle.className = "circle"
+                text.innerHTML = days[i]
+                circle.classList.add("circle")
+                if (data[days[i]] != undefined) {
+                  circle.classList.add("filled_circle")
+                  circle.onclick = function() {
+                    if (rectangle_message.style.display == "" || rectangle_message.style.display == "none") {
+                      rectangle_message.innerText = data[this.innerText]
+                      rectangle_message.style.display = "block"
+                    } else {
+                      rectangle_message.style.display = "none"
+                    }
+
+                  }
+                }
                 circle.appendChild(text);
-                node.appendChild(circle.cloneNode(true))
+                node.appendChild(circle)
         }
+
+
 }
+
+// for (var i=0;i< months.lenght; i++){
+//   if(months[i]%2==0){
+//     numdays=31
+//   }
+//   else if(months[i]%2!=0){
+//     numdays=30
+//   }
+// }
