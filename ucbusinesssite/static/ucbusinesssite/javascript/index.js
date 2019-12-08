3cb8947b0cc4c136e64f5ab9cdb0aac0d5cd281f
Joana Sousa <49571850+jbtsousa@users.noreply.github.com>
2019-12-08 19:14:37 +0000

    Add files via upload

--- ucbusinesssite/static/ucbusinesssite/javascript/index.js
+++ ucbusinesssite/static/ucbusinesssite/javascript/index.js
@@ -1,6 +1,7 @@
 window.onload = function () {
         var node = document.getElementById("rectangle")
         var months= ["jan","fev","mar","apr","may","jun","jul","aug","set","oct","nov", "dec"]
+        var days=["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31"]
 
         for (var i = 0; i < 12; ++i) {
                 var circle = document.createElement("div")
@@ -9,6 +10,5 @@ window.onload = function () {
                 circle.className = "circle"
                 circle.appendChild(text);
                 node.appendChild(circle.cloneNode(true))
-
         }
 }
