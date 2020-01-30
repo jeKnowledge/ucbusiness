53eee5df674fdbd81fb849b658384128b2e65cc6
joaofaccardoso <joaofaccardoso@gmail.com>
2020-01-30 00:34:20 +0000

    dinamic members, dinamic news, almost done with news

--- ucbusinesssite/urls.py
+++ ucbusinesssite/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('',views.LandingPage.as_view(), name='index'),
     path('about/', views.AboutPage.as_view(), name='about'),
     path('insights/', views.NewsPage.as_view(), name='insights'),
+    path('insights/<str:title>/',views.NewsArticlePage.as_view(), name='newsArticle'),
     path('ucinvest/', views.UCInvestPage.as_view(), name='ucInvest'),
     path('ucpartnership/', views.UCPartnerShipPage.as_view(), name='ucPartnership'),
     path('ucvalue/', views.UCValuePage.as_view(), name='ucValue'),
--- ucbusinesssite/views.py
+++ ucbusinesssite/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import View
-from .models import NewsArticle, Role, Member
+from .models import NewsArticle, Member
 import datetime
 
 
@@ -8,49 +8,86 @@ class LandingPage(View):
     template_name = 'ucbusinesssite/index.html'
 
     def get(self, request):
-        news = NewsArticle.objects.all()
+        context = {}
+        context['language'] = request.session.get('language', 'EN')
+        news = NewsArticle.objects.all().order_by('-datePosted')
+        if news:
+            newsArticle1 = news[0]
+            context['newsArticle1'] = newsArticle1
+            if newsArticle1.imageurl_set.all():
+                context['image1'] = newsArticle1.imageurl_set.all().first()        
         month = datetime.datetime.now().strftime('%B').upper()
-        return render(request, self.template_name, {'month': month,'newsArticles': news})
+        context['month'] = month
+        return render(request, self.template_name, context)
 
 
 class AboutPage(View):
     template_name = 'ucbusinesssite/about_us.html'
 
     def get(self, request):
-        return render(request, self.template_name)
+        context = {}
+        context['language'] = request.session.get('language', 'EN')
+        members = Member.objects.all()
+        if members:
+            context['topMember'] = members.first()
+            context['members'] = members[1:]
+        return render(request, self.template_name, context)
 
 
 class NewsPage(View):
     template_name = 'ucbusinesssite/insight.html'
 
     def get(self, request):
-        news = NewsArticle.objects.all().order_by('datePosted')
-        return render(request, self.template_name, {'newsArticles': news})
+        context = {}
+        context['language'] = request.session.get('language', 'EN')
+        news = NewsArticle.objects.all()
+        if news:
+            context['news'] = news.order_by('-datePosted')
+        return render(request, self.template_name, context)
 
 
 class UCInvestPage(View):
     template_name = 'ucbusinesssite/ucinvest.html'
 
     def get(self, request):
-        return render(request, self.template_name)
+        context = {}
+        context['language'] = request.session.get('language', 'EN')
+        return render(request, self.template_name, context)
 
 
 class UCPartnerShipPage(View):
     template_name = 'ucbusinesssite/ucpartnership.html'
 
     def get(self, request):
-        return render(request, self.template_name)
+        context = {}
+        context['language'] = request.session.get('language', 'EN')
+        return render(request, self.template_name, context)
 
 
 class UCValuePage(View):
     template_name = 'ucbusinesssite/ucvalue.html'
 
     def get(self, request):
-        return render(request, self.template_name)
+        context = {}
+        context['language'] = request.session.get('language', 'EN')
+        return render(request, self.template_name, context)
 
 
 class ContactsPage(View):
     template_name = 'ucbusinesssite/contacts.html'
 
     def get(self, request):
-        return render(request, self.template_name)
+        context = {}
+        context['language'] = request.session.get('language', 'EN')
+        return render(request, self.template_name, context)
+
+
+class NewsArticlePage(View):
+    template_name = 'ucbusinesssite/new.html'
+
+    def get(self, request, title):
+        context = {}
+        context['language'] = request.session.get('language', 'EN')
+        newsArticle = NewsArticle.objects.get(title=title)
+        context['newsArticle'] = newsArticle
+        return render(request, self.template_name, context)
