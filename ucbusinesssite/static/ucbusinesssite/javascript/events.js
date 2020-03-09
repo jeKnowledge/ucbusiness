787d1f6626558a63db81b924d9f23e4e2458f26e
Joana Brito Traças Sousa <jbtsousa@gmail.com>
2020-03-09 15:21:15 +0000

    changed timeline javascript , styled timeline buttons

--- ucbusinesssite/static/ucbusinesssite/javascript/events.js
+++ ucbusinesssite/static/ucbusinesssite/javascript/events.js
@@ -5,38 +5,46 @@ if(document.readyState === "complete" || (document.readyState!== "loading" && !d
 }
 
 function main() {
+
+    var node = document.getElementById("rectangle")
+    var numdays=30;
+
     var data = {
       // "6": "Inauguração Vice-Reitoria Pólo II e Lançamento do UC Business e Lançamento ASUC Ensino Secundário",
       // "11": "Apresentação do projeto Fit4work"
-      "10":"Assinatura Protocolo UniLoop com Fundação Calouste Gulbenkian, EGF, The Loop Company e Universidade de Coimbra - 12h"
+      "10":"Assinatura Protocolo UniLoop com Fundação Calouste Gulbenkian, EGF, The Loop Company e Universidade de Coimbra - 12h",
+      "25":"outro",
+      "12":"mais um"
     }
 
-    var node = document.getElementById("rectangle")
-    var months= ["jan","fev","mar","apr","may","jun","jul","aug","set","oct","nov", "dec"]
-    var days=["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31"]
-    var numdays=31;
-    var rectangle_message = document.querySelector("#rectangle_message")
+    var rect_message = document.querySelector("#rectangle_message")
 
-    for (var i = 0; i < numdays; ++i) {
+
+    for (var i = 1; i <= numdays; ++i) {
+
             var circle = document.createElement("div")
-            var text = document.createElement("span")
-            var hour= document.createElement("span")
+            var day_txt = document.createElement("span")
 
-            text.innerHTML = days[i]
+            var day= i.toString();
+            day_txt.innerHTML = day;
+
             circle.classList.add("circle")
-            if (data[days[i]] != undefined) {
+
+            if (data[i] != undefined) {
               circle.classList.add("filled_circle")
 
-              circle.onmouseover = function()  {
-                if (rectangle_message.style.display == "" || rectangle_message.style.display == "none") {
-                  rectangle_message.innerText = data[this.innerText]
-                  rectangle_message.style.display = "block"
+              day_txt.onmouseover  = function()  {
+
+                  if (rect_message.style.display == "" || rect_message.style.display == "none") {
+                  rect_message.innerText = data[this.innerText]
+                  rect_message.style.display = "block"
                 } else {
                   rectangle_message.style.display = "none"
                 }
               }
+
             }
-            circle.appendChild(text);
+            circle.appendChild(day_txt);
             node.appendChild(circle)
     }
 }
