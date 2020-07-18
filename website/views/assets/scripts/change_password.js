03ba3753d176c0d52ee6d31970d1f1643e22d8ed
João Cardoso <joaofaccardoso@gmail.com>
2020-07-18 17:19:40 +0100

    changed the display style of the password input

--- website/views/assets/scripts/change_password.js
+++ website/views/assets/scripts/change_password.js
@@ -8,13 +8,14 @@ function main() {
     password_label = document.getElementById("passwordLabel");
 
     if (checkbox) {
+        password_label.style.display = 'none';
         checkbox.addEventListener('change', function () {
                 if (this.checked) {
-                    password_label.hidden = false;
+                    password_label.style.display = 'block';
                     password_input.setAttribute('type', 'password');
                     password_input.required = true;
                 } else {
-                    password_label.hidden = true;
+                    password_label.style.display = 'none';
                     password_input.setAttribute('type', 'hidden');
                     password_input.required = false;
                 }
